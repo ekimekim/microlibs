35db2fa684bca669fe2c2f060a40d1dfca35138f
Mike Lang <michael@mixpanel.com>
2015-03-31 16:55:32 +0000

    Add missing dependency

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ setup(
 	author_email = "mikelang3000@gmail.com",
 	packages = ['microlibs'],
 	entry_points = {'console_scripts':['microlibs = microlibs.main:cli']},
+	install_requires = [
+		'argh',
+	]
 )
